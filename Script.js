00947e44ea2f6a94dfd9f354036a65c778a486fe
Axiser-404 <72988849+Axiser-404@users.noreply.github.com>
2020-11-14 16:59:20 +0200

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,22 @@
+const Rollbutton = document.querySelectorAll("button")[0]
+var MyAudio = document.getElementById("audio")
+
+Rollbutton.onclick = function(){
+    roll()
+    Play()
+}
+
+function roll(){
+
+    const firstRandomNumber = Math.floor(Math.random() * 6 + 1)
+    const secondRandomNumber = Math.floor(Math.random() * 6 + 1)
+
+    document.querySelectorAll("h1")[0].innerHTML = firstRandomNumber.toString()
+    document.querySelectorAll("h1")[1].innerHTML = secondRandomNumber.toString()
+    document.querySelectorAll("h1")[2].innerHTML = firstRandomNumber + secondRandomNumber
+
+}
+
+function Play(){
+    MyAudio.play()
+}
